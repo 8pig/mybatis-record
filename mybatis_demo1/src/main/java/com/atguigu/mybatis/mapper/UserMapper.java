5d1f04d5d7097ce28d5df6ca9e2df3054554d304
8pig <zhou773@foxmail.com>
2024-11-28 11:51:42 +0800

    映射开发

--- mybatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ mybatis_demo1/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -1,8 +1,17 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     /**
      * 添加用户信息
      */
     int insertUser();
+
+    /*
+    * 查询所有用户
+    * */
+    List<User> selectAll();
 }
